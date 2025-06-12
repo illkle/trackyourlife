d030ceeac34adaa5b20c87f870d4c3d4d502b604
Kirill Kleymenov <k.kelolov@gmail.com>
2025-06-13 00:06:09 +0300

    tailwind fix

--- apps/server/vite.config.ts
+++ apps/server/vite.config.ts
@@ -1,37 +1,13 @@
-// vite.config.ts
-//import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  /*
-  optimizeDeps: {
-    esbuildOptions: {
-      target: "es2022",
-      supported: {
-        "top-level-await": true,
-      },
-    },
-  },
-
-  build: {
-    target: "es2022",
-  },
-  esbuild: {
-    target: "es2022",
-    supported: {
-      "top-level-await": true,
-    },
-  },
-  */
-
   plugins: [
     tsconfigPaths({ projects: ["./tsconfig.json"] }),
 
-    //  tailwindcss(),
-    tanstackStart({
-      /** Add your options here */
-    }),
+    tailwindcss(),
+    tanstackStart({}),
   ],
 });
