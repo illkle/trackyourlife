b9558bdb9b9fa3b93251be38db5ef868d923168a
Kirill Kleymenov <k.kelolov@gmail.com>
2025-09-22 22:56:11 +0300

    tanstack update and fix docker for it

--- apps/server/vite.config.ts
+++ apps/server/vite.config.ts
@@ -1,5 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -12,6 +13,7 @@ export default defineConfig({
     tsconfigPaths({ projects: ["./tsconfig.json"] }),
 
     tailwindcss(),
-    tanstackStart({}),
+    tanstackStart({ customViteReactPlugin: true }),
+    viteReact(),
   ],
 });
