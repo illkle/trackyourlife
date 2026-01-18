9d931803403a09e5cedafd25038453a81b9eab19
Kirill Kleymenov <k.kelolov@gmail.com>
2026-01-19 00:24:22 +0300

    migration fix

--- packages/db/drizzle/0043_loving_shinobi_shaw.sql
+++ packages/db/drizzle/0043_loving_shinobi_shaw.sql
@@ -1,5 +1,7 @@
 DROP TABLE "TYL_trackableRecordAttributes" CASCADE;--> statement-breakpoint
+DELETE FROM "TYL_trackable" WHERE "type" IN ('tags', 'logs');--> statement-breakpoint
 ALTER TABLE "TYL_trackable" ALTER COLUMN "type" SET DATA TYPE text;--> statement-breakpoint
+
 DROP TYPE "public"."type";--> statement-breakpoint
 CREATE TYPE "public"."type" AS ENUM('boolean', 'number', 'text');--> statement-breakpoint
 ALTER TABLE "TYL_trackable" ALTER COLUMN "type" SET DATA TYPE "public"."type" USING "type"::"public"."type";--> statement-breakpoint
